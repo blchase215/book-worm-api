151f1b1b5a813fff861ba6d076c94a6df74590f2
Brian Chase <brianchasemusic@agmail.com>
2022-10-03 00:51:26 -0400

    added REMOVE_BOOK mutation using typeDefs as reference

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -42,4 +42,20 @@ export const SAVE_BOOK = gql`
   }
 `;
 
-export const REMOVE_BOOK = gql``;
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
