e684c62d286471ef2fdf02017a970fc4fbd72662
Brian Chase <brianchasemusic@agmail.com>
2022-10-02 22:49:12 -0400

    added in typeDefs, used module 21 activity 6 as a guide, started mutations.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,37 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User!
+  }
+
+  type Mutations {
+    login:
+    addUser:
+    saveBook:
+    removeBook:
+  }
+`
